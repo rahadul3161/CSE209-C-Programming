094563fddaaf7d1bb6a7a0878fd2625fd2553da0
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-03-05 19:46:26 +0600

    Update and rename 2. factorialOf12.java to 2. Factorial.java

--- Java Assignments/Assignment 2/2. Factorial.java
+++ Java Assignments/Assignment 2/2. Factorial.java
@@ -1,6 +1,6 @@
 //Write a Java program to calculate factorial of 12.
 
-public class factorialOf12 {
+public class Factoril {
   public static void main(String[] args) {
     int fact=1, number=12;
     for(int i=1; i<=number; i++) {
